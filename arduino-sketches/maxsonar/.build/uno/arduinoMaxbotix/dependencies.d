48ef0f998c680ebd51cb20d056fccc9d9cf21f55
Alain <hello@alainbarthelemy.com>
2014-05-01 14:50:24 +0000

    arduino-sketches

---
+++ arduino-sketches/maxsonar/.build/uno/arduinoMaxbotix/dependencies.d
@@ -0,0 +1,28 @@
+.build/uno/arduinoMaxbotix/Maxbotix.d .build/uno/arduinoMaxbotix/Maxbotix.o: /usr/share/arduino/libraries/arduinoMaxbotix/Maxbotix.cpp \
+ /usr/share/arduino/libraries/arduinoMaxbotix/Maxbotix.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ /usr/share/arduino/libraries/arduinoMaxbotix/RxSoftwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h
+.build/uno/arduinoMaxbotix/RxSoftwareSerial.d .build/uno/arduinoMaxbotix/RxSoftwareSerial.o: \
+ /usr/share/arduino/libraries/arduinoMaxbotix/RxSoftwareSerial.cpp \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ /usr/share/arduino/libraries/arduinoMaxbotix/RxSoftwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h
---
+++ arduino-sketches/maxsonar/.build/uno/src/sketch.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include "Maxbotix.h"
+void setup();
+void loop();
+#line 1 "src/sketch.ino"
+/*
+  Maxbotix simple test
+
+  Instructions:
+  - At least one of: (comment the appropriate code below)
+    * PW is digital pin 8
+    * TX is digital pin 6
+    * AN is analog pin A0
+  - Change code below according to your model (LV, XL and
+  HRLV supported)
+
+  Note:
+  For convenience, the getRange method will always return centimeters.
+  You can use convert fuctions to convert to another unit (toInches and
+  toCentimeters are available)
+
+*/
+//#include "Maxbotix.h"
+
+Maxbotix rangeSensorPW(8, Maxbotix::PW, Maxbotix::LV);
+//Maxbotix rangeSensorTX(6, Maxbotix::TX, Maxbotix::LV);
+//Maxbotix rangeSensorAD(A0, Maxbotix::AN, Maxbotix::LV);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  unsigned long start;
+  
+  //Serial.print("PW: ");
+  //float range = rangeSensorPW.getRange() ;
+  //Serial.print(range);
+  
+  //int thisPitch = map(range, 300, 10, 120, 1500);
+  //tone(9, thisPitch, 30);
+  
+  Serial.println();
+  delay(50);
+}
