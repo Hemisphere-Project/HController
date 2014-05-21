14bfee7af8033881bd2e743ca41a059705602223
Alain <hello@alainbarthelemy.com>
2014-05-21 18:59:27 +0200

    arduino

---
+++ arduino-sketches/distToVolume/.build/micro/arduino/HID.d
@@ -0,0 +1,17 @@
+.build/micro/arduino/HID.d .build/micro/arduino/HID.o:  \
+ /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/HID.cpp \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Platform.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Arduino.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/binary.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WCharacter.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/WString.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/HardwareSerial.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Stream.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Print.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/Printable.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/USBAPI.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/variants/micro/pins_arduino.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/variants/micro/../leonardo/pins_arduino.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/USBDesc.h \
+  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/USBCore.h
---
+++ arduino-sketches/distToVolume/.build/micro/src/sketch.cpp
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+#include "Maxbotix.h"
+#include <stdlib.h>
+void setup();
+void loop();
+#line 1 "src/sketch.ino"
+//#include "Maxbotix.h"
+//#include <stdlib.h>
+
+
+Maxbotix rangeSensorPW(8, Maxbotix::PW, Maxbotix::LV);
+
+const float rangeMin = 80;// cm
+const float rangeMax = 110;// cm
+const float volMin = 20;// 0 - 100
+const float volMax = 50;// 0 - 100
+const int d = 50; // ms
+
+float GLITCH_DIFF = 100;
+float range;//measured ranges
+float prevRange;//measured ranges
+float volume = 0;
+float stepVolume = 0;
+float lastStepVolume = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  range = prevRange = rangeSensorPW.getRange(); 
+  volume = stepVolume = lastStepVolume = volMin;
+}
+
+void loop()
+{
+  
+  // PW range in cm from the sensor
+  range = rangeSensorPW.getRange() ;
+  
+  
+  // filtering
+  if(abs(range-prevRange) > GLITCH_DIFF){
+  		float tmp = range;  
+  		range = prevRange;
+  		prevRange = tmp;
+  }
+  
+  
+ 
+  volume = constrain(range,rangeMin,rangeMax);
+  volume = map(volume,rangeMax,rangeMin,volMin,volMax);
+  
+  if(volume > stepVolume) stepVolume++;
+  if(volume < stepVolume) stepVolume--;
+  
+  if(stepVolume == lastStepVolume)
+  	  return;
+  
+  Serial.print("{\"name\":\"volume\",\"args\":{\"value\":");
+  Serial.print(stepVolume);
+  Serial.print(",\"range\":");
+  Serial.print(range);
+  Serial.print("}}");
+  Serial.println();
+  
+  lastStepVolume = stepVolume;
+  
+  delay(d);
+}
