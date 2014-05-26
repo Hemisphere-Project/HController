de125c046070953c76d0250b0e16afdecb500d5d
Alain <hello@alainbarthelemy.com>
2014-05-26 16:27:30 +0200

    arduino

--- arduino-sketches/distToVolume/.build/micro/arduino/new.d
+++
@@ -1,3 +0,0 @@
-.build/micro/arduino/new.d .build/micro/arduino/new.o:  \
- /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.cpp \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/arduino/cores/arduino/new.h
